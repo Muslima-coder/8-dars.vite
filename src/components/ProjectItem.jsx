1c13227d521f0bf2a2dede14dead3eebdacf3415
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-07 02:19:15 +0500

    update commit

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -6,10 +6,10 @@ import Text from "./Text";
 function ProjectItem ({item}) {
     return (
         <div className="sm:w-[550px] mx-auto sm:mx-0 w-[300px] shadow-lg shadow-slate-300 p-[25px] rounded-tl-[54px] rounded-br-[54px]">
-            <img className="w-[270px] sm:w-[500px] sm:h-[286px]" src={item.img} alt="img" width={500} height={286} />
+            <img className="w-[270px] sm:w-[500px] sm:h-[286px] mb-[10px] sm:mb-[25px]" src={item.img} alt="img" width={500} height={286} />
             <Heading extraClass={"!text-center"} title={item.title}/>
             <Text title={item.content}/>
-            <Button extraClass={"!py-[15px] !px-[52px] "} title={"Look it up"}/>
+            <Button extraClass={"!py-[15px] !px-[52px] !sm:mx-0 !mx-auto "} title={"Look it up"}/>
         </div>
     )
 }
