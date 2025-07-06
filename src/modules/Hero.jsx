a50f7a2183c9371e0ad5e382e3f6bba01dd5e67f
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-07 01:34:19 +0500

    update commit

--- src/modules/Hero.jsx
+++ src/modules/Hero.jsx
@@ -6,7 +6,7 @@ function Hero(){
     return(
         <section className="hero-section py-[100px]  sm:pt-[157px] sm:pb-[250px]">
             <div className="containers">
-            <div className="sm:w-[850px] sm:mx-0 sm:text-start text-center mx-auto w-[400px] ">
+            <div className="sm:w-[850px] sm:mx-0 sm:text-start text-center mx-auto w-[280px] ">
             <Heading title={"Lorem ipsum dolor sit amet"}/>
             <Text title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}/>
             <Button title={"Let’s begin"}/>
