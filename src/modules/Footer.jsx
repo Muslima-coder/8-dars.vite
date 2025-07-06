c33a1ea9f381b55d2b5e336de67efe632e0bad8b
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-07 02:51:57 +0500

    update commit

--- src/modules/Footer.jsx
+++ src/modules/Footer.jsx
@@ -7,7 +7,7 @@ function Footer(){
         <footer className="pb-[80px]">
             <div className="containers">
 
-            <div className="flex sm:w-full w-[400px] mx-auto sm:mx-0 flex-wrap justify-between items-center">
+            <div className="flex sm:w-full w-[200px] sm:gap-0 gap-[20px] !flex-wrap mx-auto sm:mx-0  justify-between items-center">
                 <div>
                 <Text extraClass={"!text-[rgba(255,255,255,1)] !mb-0"} title={"Call me:"}/>
                 <Text extraClass={"!text-[rgba(255,255,255,1)] !mb-0"} title={"123-456-789"}/>
@@ -16,7 +16,7 @@ function Footer(){
                 <Text extraClass={"!text-[rgba(255,255,255,1)] !mb-0"} title={"Email:"}/>
                 <Text extraClass={"!text-[rgba(255,255,255,1)] !mb-0"} title={"xxx@xyz.com"}/>
                 </div>
-                <div className="flex gap-[15px] sm:gap-[65px] items-center">
+                <div className="sm:mx-0 mx-auto flex gap-[15px] sm:gap-[65px] items-center">
                     <GitHubIcon2/>
                     <LinKedIcon2/>
                     <FigmaIcon2/>
