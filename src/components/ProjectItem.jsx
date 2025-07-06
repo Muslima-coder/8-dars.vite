d79be6181a674e1065a40b1a578bb5eb13a4c4f5
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-07 02:23:15 +0500

    update commit

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -8,8 +8,8 @@ function ProjectItem ({item}) {
         <div className="sm:w-[550px] mx-auto sm:mx-0 w-[300px] shadow-lg shadow-slate-300 p-[25px] rounded-tl-[54px] rounded-br-[54px]">
             <img className="w-[270px] sm:w-[500px] sm:h-[286px] mb-[10px] sm:mb-[25px]" src={item.img} alt="img" width={500} height={286} />
             <Heading extraClass={"!text-center"} title={item.title}/>
-            <Text title={item.content}/>
-            <Button extraClass={"!py-[15px] !px-[52px] !sm:mx-0 !mx-auto "} title={"Look it up"}/>
+            <Text extraClass={"sm:text-start text-center"} title={item.content}/>
+            <Button extraClass={"!py-[15px] !px-[52px] !flex !sm:justify-start !justify-center sm:mx-0 mx-auto "} title={"Look it up"}/>
         </div>
     )
 }
